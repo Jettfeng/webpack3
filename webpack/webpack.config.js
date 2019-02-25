5b3430965234ac000e7aaf84c79cf4977ac74722
1130034920@qq.com <1130034920@qq.com>
2019-02-25 20:44:00 +0800

    3-6 由浅入深 webpack - 打包公共代码（2）

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,9 @@ var webpack = require("webpack"); //局部webpack作为依赖，全局webpack作
 var path = require("path");
 module.exports = {
   entry: {
-    pageA: "./src/pageA.js"
+    pageA: "./src/pageA.js",
+    pageB: "./src/pageB.js",
+    vendor: ["lodash"] //第三方包
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
@@ -11,8 +13,24 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
+      //提取公共代码
       name: "common",
-      minChunks: 2
+      minChunks: 2,
+      chunks: ["pageA", "pageB"]
+    }),
+    // 合并vendor和manifest
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ["vendor", "manifest"],
+      minChunks: Infinity
     })
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   //提取公共模块是针对多个entry,单个entry不起作用
+    //   name: "vendor", //将lodash和公共代码打包到一起
+    //   minChunks: Infinity
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "manifest", //把webpack生成的代码和vendor分开
+    //   minChunks: Infinity
+    // })
   ]
 };
